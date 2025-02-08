66c57fab8a4983976a3e8ec0bd445d97665fdf50
Abhinay19423 <148855595+Abhinay19423@users.noreply.github.com>
2025-02-08 20:52:07 +0530

    now added changes to file correctly

--- dbconnection/src/main/java/com/mydb/dbconnection/component/StudentDAO.java
+++ dbconnection/src/main/java/com/mydb/dbconnection/component/StudentDAO.java
@@ -9,9 +9,12 @@ public interface StudentDAO {
     // void save(Student stud);
     Student getDetail(int id);
     List<Student> getDetails();
+<<<<<<< HEAD
 
     void updateDetails(int id);
     // diff_method_to_retrive
     List<Student> type1();
     List<Student> type2(String name);
+=======
+>>>>>>> parent of c1bd69c (updating the values in the student table)
 }
